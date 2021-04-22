8781c2189b6c313b52c84a45c337f13d7dc8579a
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-04-22 21:44:40 +0100

    Reduce memory usage.

--- project/app/models/fastestchange.py
+++ project/app/models/fastestchange.py
@@ -23,52 +23,55 @@ class FastestChange(models.Model):
         last_pressure_positive, last_pressure_negative = None, None
 
         history = []
-        for row in WeatherRow.objects.all():
-            history.append(row)
-            while history[0].date < row.date - timedelta(hours=1):
-                del history[0]
+        row_count = WeatherRow.objects.count()
+        print "Loading", row_count, "rows"
+        for offset in range(0, row_count, 1000):
+            for row in WeatherRow.objects.all()[offset:offset+1000]:
+                history.append(row)
+                while history[0].date < row.date - timedelta(hours=1):
+                    del history[0]
 
-            first_temp_out = None
-            for h in history:
-                if h.temp_out is not None:
-                    first_temp_out = h
-                    break
+                first_temp_out = None
+                for h in history:
+                    if h.temp_out is not None:
+                        first_temp_out = h
+                        break
 
-            last_temp_out = None
-            for h in history[::-1]:
-                if h.temp_out is not None:
-                    last_temp_out = h
-                    break
+                last_temp_out = None
+                for h in history[::-1]:
+                    if h.temp_out is not None:
+                        last_temp_out = h
+                        break
 
-            if last_temp_out is not None and first_temp_out.temp_out < last_temp_out.temp_out:
-                if last_temp_negative is not None:
-                    temp_negatives.append(last_temp_negative)
-                    last_temp_negative = None
+                if last_temp_out is not None and first_temp_out.temp_out < last_temp_out.temp_out:
+                    if last_temp_negative is not None:
+                        temp_negatives.append(last_temp_negative)
+                        last_temp_negative = None
 
-                if last_temp_positive is None or (last_temp_out.temp_out - first_temp_out.temp_out) > last_temp_positive.value:
-                    last_temp_positive = FastestChange(date=FastestChange.get_date(history[0]), temperature=True, positive=True, value=last_temp_out.temp_out - first_temp_out.temp_out, end=history[-1].date)
-            elif last_temp_out is not None and first_temp_out.temp_out > last_temp_out.temp_out:
-                if last_temp_positive is not None:
-                    temp_positives.append(last_temp_positive)
-                    last_temp_positive = None
+                    if last_temp_positive is None or (last_temp_out.temp_out - first_temp_out.temp_out) > last_temp_positive.value:
+                        last_temp_positive = FastestChange(date=FastestChange.get_date(history[0]), temperature=True, positive=True, value=last_temp_out.temp_out - first_temp_out.temp_out, end=history[-1].date)
+                elif last_temp_out is not None and first_temp_out.temp_out > last_temp_out.temp_out:
+                    if last_temp_positive is not None:
+                        temp_positives.append(last_temp_positive)
+                        last_temp_positive = None
 
-                if last_temp_negative is None or (last_temp_out.temp_out - first_temp_out.temp_out) < last_temp_negative.value:
-                    last_temp_negative = FastestChange(date=FastestChange.get_date(history[0]), temperature=True, positive=False, value=last_temp_out.temp_out - first_temp_out.temp_out, end=history[-1].date)
+                    if last_temp_negative is None or (last_temp_out.temp_out - first_temp_out.temp_out) < last_temp_negative.value:
+                        last_temp_negative = FastestChange(date=FastestChange.get_date(history[0]), temperature=True, positive=False, value=last_temp_out.temp_out - first_temp_out.temp_out, end=history[-1].date)
 
-            if history[0].abs_pressure < history[-1].abs_pressure:
-                if last_pressure_negative is not None:
-                    pressure_negatives.append(last_pressure_negative)
-                    last_pressure_negative = None
+                if history[0].abs_pressure < history[-1].abs_pressure:
+                    if last_pressure_negative is not None:
+                        pressure_negatives.append(last_pressure_negative)
+                        last_pressure_negative = None
 
-                if last_pressure_positive is None or (history[-1].abs_pressure - history[0].abs_pressure) > last_pressure_positive.value:
-                    last_pressure_positive = FastestChange(date=FastestChange.get_date(history[0]), temperature=False, positive=True, value=history[-1].abs_pressure - history[0].abs_pressure, end=history[-1].date)
-            elif history[0].abs_pressure > history[-1].abs_pressure:
-                if last_pressure_positive is not None:
-                    pressure_positives.append(last_pressure_positive)
-                    last_pressure_positive = None
+                    if last_pressure_positive is None or (history[-1].abs_pressure - history[0].abs_pressure) > last_pressure_positive.value:
+                        last_pressure_positive = FastestChange(date=FastestChange.get_date(history[0]), temperature=False, positive=True, value=history[-1].abs_pressure - history[0].abs_pressure, end=history[-1].date)
+                elif history[0].abs_pressure > history[-1].abs_pressure:
+                    if last_pressure_positive is not None:
+                        pressure_positives.append(last_pressure_positive)
+                        last_pressure_positive = None
 
-                if last_pressure_negative is None or (history[-1].abs_pressure - history[0].abs_pressure) < last_pressure_negative.value:
-                    last_pressure_negative = FastestChange(date=FastestChange.get_date(history[0]), temperature=False, positive=False, value=history[-1].abs_pressure - history[0].abs_pressure, end=history[-1].date)
+                    if last_pressure_negative is None or (history[-1].abs_pressure - history[0].abs_pressure) < last_pressure_negative.value:
+                        last_pressure_negative = FastestChange(date=FastestChange.get_date(history[0]), temperature=False, positive=False, value=history[-1].abs_pressure - history[0].abs_pressure, end=history[-1].date)
 
         if last_temp_negative is not None:
             temp_negatives.append(last_temp_negative)
@@ -92,6 +95,8 @@ class FastestChange(models.Model):
         pressure_positives.sort(key=lambda x: -x.value)
         [x.save() for x in pressure_positives[:5]]
         pressure_negatives.sort(key=lambda x: x.value)
+        for fc in pressure_negatives[:5]:
+            print fc.date, fc.temperature, fc.positive, fc.value, fc.end
         [x.save() for x in pressure_negatives[:5]]
 
     @staticmethod
